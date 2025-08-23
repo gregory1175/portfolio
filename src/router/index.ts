ae95530bb66818fde1b31865d35ab381eef1be24
gregory <gredmyr@gmail.com>
2025-08-23 03:30:06 +0300

    Update project configuration files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Portfolio from '@/templates/homePortfolio.vue'
+import Main from '@/templates/homeMain.vue'
+import Links from '@/templates/linksPage.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: Main,
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: Portfolio,
+    },
+    {
+      path: '/links',
+      name: 'links',
+      component: Links,
+    },
+  ],
 })
 
 export default router
